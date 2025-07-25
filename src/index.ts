0834ed3e51281af21c46f4aec10e58a805d2946b
Luis <troyaluis56@gmail.com>
2025-07-24 22:10:18 -0500

    feat: wip

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { Hono } from 'hono'
 
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+const schema = z.object({
+  name: z.string(),
+  age: z.number(),
+})
+
 const app = new Hono()
 
 app.get('/', (c) => {
+  console.log('Getting products...')
   // Give me 10 products with name, price, description and stock
   const products = Array.from({ length: 10 }, (_, i) => ({
     id: i + 1,
@@ -12,6 +21,18 @@ app.get('/', (c) => {
     stock: Math.floor(Math.random() * 100) + 1,
   }))
   return c.json({products})
+})
+
+app.get('/register', zValidator('json', schema), (c) => {
+  const data = c.req.valid('json')
+
+  console.log('Registering user...')
+  console.log('Data:', data)
+
+  return c.json({
+    message: 'User registered successfully',
+    data
+  })
 })
 
 export default app
